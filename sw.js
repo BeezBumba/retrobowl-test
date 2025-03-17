d1785d7274b3270caa4b1bb90f813bb9f2719446
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2025-03-16 20:07:08 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const KEY = 'RETROBOWLCOLLEGE';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('message', (event) => {
+    if (event.data.type === 'CACHE_URLS') {
+        event.waitUntil(
+            caches.open(KEY)
+                .then((cache) => {
+                    return cache.addAll(event.data.payload);
+                })
+        );
+    }
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      try {
+        console.log(`[Service Worker] Attempting to serve resource from cache: ${e.request.url}`);
+        const r = await caches.match(e.request);
+        if (r) {
+          return r;
+        }
+        console.log(`[Service Worker] Attempting live fetch: ${e.request.url}`);
+        const response = await fetch(e.request, { mode: 'no-cors' });
+        const cache = await caches.open(KEY);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+      } catch (err) {
+        console.error(`[Service Worker] Fetch failed: ${err}`);
+      }
+    })()
+  );
+});
